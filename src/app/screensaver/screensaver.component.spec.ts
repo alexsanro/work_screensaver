06c8e8828b44a2c4e9359ddc67e2aacde1111334
alexsanro <alexsanro1@gmail.com>
2020-02-06 00:10:26 +0100

    Fixed test and refactor Add spec reporter to karma

--- src/app/screensaver/screensaver.component.spec.ts
+++ src/app/screensaver/screensaver.component.spec.ts
@@ -32,23 +32,6 @@ describe('ScreensaverComponent', () => {
     fixture.detectChanges();
   });
 
-  //MAIN.JS MOCKUP NEW WINDOW
-  beforeEach(() => {
-    var electronService = new ElectronService();
-    var displays = electronService.remote.screen.getAllDisplays();
-    console.log("Displays = " + displays.length)
-    displays.forEach((value, key) => {
-      windowSaver[key] = new electronService.remote.BrowserWindow({})
-
-      windowSaver[key].loadURL(url.format({
-        pathname: path.join(__dirname, 'dist/index.html'),
-        protocol: 'file:',
-        slashes: true,
-        hash: '/screensaver/coffee_cup.gif'
-      }));
-    })
-  })
-
   afterEach(() => {
     windowSaver = [];
   })
@@ -59,46 +42,34 @@ describe('ScreensaverComponent', () => {
 
 
   it('Close windows with keydown', function () {
-    var arrayWindows = windowSaver;
-    console.log("KEYDOWN: " + new ElectronService().remote.BrowserWindow.getAllWindows().length)
+    var spy = false;
     spyOn(ipcRenderer, 'send').and.callFake(function () {
-      arrayWindows.forEach((element, key) => {
-        arrayWindows.splice(key, 1);
-        element.destroy();
-      });
+      spy = true;
     });
 
     document.dispatchEvent(new Event('keydown'));
-    expect(arrayWindows.length).toEqual(0);
+    expect(spy).toEqual(true);
   });
 
   it('Close windows with mousedown', function () {
-    console.log("MOUSEDOWN: " + new ElectronService().remote.BrowserWindow.getAllWindows().length)
-    var arrayWindows = windowSaver;
+    var spy = false;
     spyOn(ipcRenderer, 'send').and.callFake(function () {
-      arrayWindows.forEach((element, key) => {
-        arrayWindows.splice(key, 1);
-        element.destroy();
-      });
+      spy = true;
     });
     document.dispatchEvent(new Event('mousedown'));
     
-    expect(arrayWindows.length).toEqual(0);
+    expect(spy).toEqual(true);
   });
 
   it('Close windows with mousemove', (done) => {
-    console.log("MOUSEMOVE: " + new ElectronService().remote.BrowserWindow.getAllWindows().length)
-    var arrayWindows = windowSaver;
+    var spy = false;
     spyOn(ipcRenderer, 'send').and.callFake(function () {
-      arrayWindows.forEach((element, key) => {
-        arrayWindows.splice(key, 1);
-        element.destroy();
-      });
+      spy = true;
     });
 
     setTimeout(() => {
       document.dispatchEvent(new MouseEvent("mousemove", <MouseEventInit>{ movementX: 10, movementY: 10 }));
-      expect(arrayWindows.length).toEqual(0)
+      expect(spy).toEqual(true)
       done();
     }, 2100);
   });
--- src/app/screensaver/screensaver.component.ts
+++ src/app/screensaver/screensaver.component.ts
@@ -11,7 +11,7 @@ import { ActivatedRoute } from '@angular/router';
 export class ScreensaverComponent implements OnInit {
 
   urlFile = null;
-  constructor(private route?: ActivatedRoute) {}
+  constructor(private route?: ActivatedRoute) { }
 
   ngOnInit() {
     this.urlFile = this.route.snapshot.paramMap.get("file");
@@ -19,24 +19,19 @@ export class ScreensaverComponent implements OnInit {
   }
 
   closeWindowEvents() {
-    console.log("events")
     document.addEventListener('keydown', () => {
-      console.log("keydown")
       ipcRenderer.send('sendCloseAllWindows');
     });
 
     document.addEventListener('mousedown', () => {
-      console.log("mousevent")
       ipcRenderer.send('sendCloseAllWindows');
     });
-    
+
     setTimeout(() => {
-      console.log("time")
       var treshold = 5;
       document.addEventListener('mousemove', (e) => {
         if (treshold * treshold < e.movementX * e.movementX
           + e.movementY * e.movementY) {
-        console.log("mouse")
           ipcRenderer.send('sendCloseAllWindows');
         }
       });
